7b6ea229d45c42a3f25228818e7457d5c9c007fa
ArkashJ <arkjain@bu.edu>
2023-03-06 21:21:11 -0500

    ticker, kill commands, make function

--- raft/raft.go
+++ raft/raft.go
@@ -3,6 +3,7 @@ package raft
 import (
 	"reflect"
 	"sync"
+	"sync/atomic"
 )
 
 // -------------------------------------------------------------------------------------------------
@@ -43,6 +44,7 @@ type Raft struct {
 }
 
 // -------------------------------------------------------------------------------------------------
+
 // return currentTerm and whether this server believes its the leader or not
 func (rf *Raft) GetState() (int, bool) {
 	var term int
@@ -50,6 +52,8 @@ func (rf *Raft) GetState() (int, bool) {
 	//code
 	return term, isLeader
 }
+
+// -------------------------------------------------------------------------------------------------
 
 type RequestVoteArgs struct {
 }
@@ -60,23 +64,72 @@ type RequestVoteReply struct {
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 }
 
+// -------------------------------------------------------------------------------------------------
+
 // send rpc to the server which is the index in rf.peers[]. The call will always return t/f
 // unless there is a problem with the handler function on the server side
 // Call() sends a request and waits, if the reply comes with a timeout, its true otherwise Call() returns false
-
 func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) {
 	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
 	return ok
 }
 
+// -------------------------------------------------------------------------------------------------
+
 // the service using raft must return the index of the value to be committed
 // return false if the server is not the lader otherwise, return the index of where the log would
 // have been committed, the currentTerm and true
-
 func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	index := -1
 	term := -1
 	isLeader := true
 
 	return index, term, isLeader
+}
+
+// -------------------------------------------------------------------------------------------------
+
+func (rf *Raft) persist() {
+}
+
+// restore previously persisted state.
+func (rf *Raft) readPersist(data []byte) {
+	if data == nil || len(data) < 1 { // bootstrap without any state?
+		return
+	}
+}
+
+// -------------------------------------------------------------------------------------------------
+// the code does not halt go routines but calls Kill(), one can check if go routines are killed by looking at the value
+// Atomic prevents the need for locking
+
+func (rf *Raft) Kill() {
+	atomic.StoreInt32(&rf.dead, 1)
+}
+
+func (rf *Raft) Killed() bool {
+	z := atomic.LoadInt32(&rf.dead)
+	return z == 1
+}
+
+// -------------------------------------------------------------------------------------------------
+
+// start a new election if the server hasnt recieved a heartbeat
+func (rf *Raft) ticker() {
+	for !rf.Killed() {
+	}
+}
+
+// -------------------------------------------------------------------------------------------------
+
+func Make(peers []*ClientEnd, me int, persister *Persister, apply chan ApplyMsg) *Raft {
+	rf := &Raft{}
+	rf.peers = peers
+	rf.me = me
+	rf.persister = persister
+
+	rf.readPersist(persister.ReadRaftState())
+	go rf.ticker()
+
+	return rf
 }
