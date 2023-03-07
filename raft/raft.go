3905c802bcd2ddf4b1958a1f47741a6addeccc18
ArkashJ <arkjain@bu.edu>
2023-03-06 20:16:45 -0500

    adding reply structs

--- raft/raft.go
+++ raft/raft.go
@@ -1,6 +1,9 @@
 package raft
 
-import "sync"
+import (
+	"reflect"
+	"sync"
+)
 
 // once a new log entry is commited, peer sends an ApplyMsg to the service via applyCh passed to Make().
 // CommandValid is set to true to indicate ApplyMsg contains a new log entry
@@ -10,6 +13,18 @@ type ApplyMsg struct {
 	CommandIndex int
 }
 
+type reqMsg struct {
+	endName  interface{}
+	svcMeth  string
+	argsType reflect.Type
+	args     []byte
+	replyCh  chan replyMsg
+}
+
+type replyMsg struct {
+	ok    bool
+	reply []byte
+}
 type ClientEnd struct {
 	endName interface{}   // this is endpoints name
 	ch      chan reqMsg   // copy if Network.endCh
@@ -17,9 +32,9 @@ type ClientEnd struct {
 }
 
 type Raft struct {
-	mu        sync.Mutex
-	peers     []*ClientEnd
-	persister *Persister
-	me        int
-	dead      int32
+	mu        sync.Mutex   // for locking
+	peers     []*ClientEnd // Rpc end points of all peers
+	persister *Persister   // object to hold this peer's persister state
+	me        int          // peers index
+	dead      int32        // sent by Kill()
 }
