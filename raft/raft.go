27c4518de683afc126c51484417cb3bdfcbe2968
ArkashJ <arkjain@bu.edu>
2023-03-06 20:21:55 -0500

    function to return the currentTerm from a server and whether it believes its the leader or not

--- raft/raft.go
+++ raft/raft.go
@@ -5,13 +5,7 @@ import (
 	"sync"
 )
 
-// once a new log entry is commited, peer sends an ApplyMsg to the service via applyCh passed to Make().
-// CommandValid is set to true to indicate ApplyMsg contains a new log entry
-type ApplyMsg struct {
-	CommandValid bool
-	Command      interface{}
-	CommandIndex int
-}
+// -------------------------------------------------------------------------------------------------
 
 type reqMsg struct {
 	endName  interface{}
@@ -31,10 +25,28 @@ type ClientEnd struct {
 	done    chan struct{} //closed when network is cleaning up
 }
 
+// -------------------------------------------------------------------------------------------------
+
+// once a new log entry is commited, peer sends an ApplyMsg to the service via applyCh passed to Make().
+// CommandValid is set to true to indicate ApplyMsg contains a new log entry
+type ApplyMsg struct {
+	CommandValid bool
+	Command      interface{}
+	CommandIndex int
+}
 type Raft struct {
 	mu        sync.Mutex   // for locking
 	peers     []*ClientEnd // Rpc end points of all peers
 	persister *Persister   // object to hold this peer's persister state
 	me        int          // peers index
 	dead      int32        // sent by Kill()
+}
+
+// -------------------------------------------------------------------------------------------------
+// return currentTerm and whether this server believes its the leader or not
+func (rf *Raft) GetState() (int, bool) {
+	var term int
+	var isleader bool
+	//code
+	return term, isleader
 }
